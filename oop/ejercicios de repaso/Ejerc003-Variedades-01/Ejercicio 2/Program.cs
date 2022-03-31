aeeebb37979ea25522a0eb088ac786129e8562c6
0horane <leandro.horane.siracusa@gmail.com>
2022-03-31 10:20:15 -0300

    termine lo de practica

--- oop/ejercicios de repaso/Ejerc003-Variedades-01/Ejercicio 2/Program.cs
+++ oop/ejercicios de repaso/Ejerc003-Variedades-01/Ejercicio 2/Program.cs
@@ -1,22 +1,70 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Globalization;
+//using System.Linq;
 namespace Ejercicio_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double salary = 1200;
-            double hours = 60;
-            double salariobruto = salary * hours;
-            Console.WriteLine($"Su salario bruto es de {salariobruto}");
-            //suponiendo que se aplica uno sobre lo que queda del otro
-            double salarioneto = salariobruto * (1 - 0.11) * (1 - 0.03);
-            Console.WriteLine($"Su salario bruto es de {salarioneto}");
-            //suponiendo que e aplican por separado sobre el salario original
-            //double salarioneto = salariobruto - salariobruto*0.11 - salariobruto*0.03;
-            //
-            //
+
+            float inputfloat(string prompt = "Ingrese un numero: ", string failprompt = "Eso no es un Numero. Ingrese un numero: ")
+            {
+                Console.WriteLine(prompt);
+                float inputnumber;
+                while (!float.TryParse(Console.ReadLine(), out inputnumber))
+                {
+                    Console.WriteLine(failprompt);
+                }
+                return inputnumber;
+            }
+
+            int inputint(string prompt = "Ingrese un numero: ", string failprompt = "Eso no es un Numero. Ingrese un numero: ")
+            {
+                Console.WriteLine(prompt);
+                int inputnumber;
+                while (!int.TryParse(Console.ReadLine(), out inputnumber))
+                {
+                    Console.WriteLine(failprompt);
+                }
+                return inputnumber;
+            }
+
+            string inputstr(string prompt = "Ingrese un string: ", string failprompt = "Eso no es un string. Ingrese un numero: ")
+            {
+                Console.WriteLine(prompt);
+                return Console.ReadLine() ;
+            }
+
+
+
+            int numdocentes = inputint("Cuantos docentes quiere ingresar?");
+            List<string> nombres = new List<string>();
+            List<float> brutos = new List<float>();
+            List<float> jubilaciones = new List<float>();
+            List<float> obrasociales = new List<float>();
+            for (int i = 0; i < numdocentes; i++)
+            {
+                nombres.Add(inputstr("Ingresa el nombre del docente"));
+                brutos.Add(inputfloat("Ingresa el salario bruto del docente"));
+                jubilaciones.Add(inputfloat("Ingresa el aporte de jubilaciones del docente (de 0 a 1)"));
+                obrasociales.Add(inputfloat("Ingresa el aporte de obra social del docente (de 0 a 1)"));
+
+                for (int j = 0; j < nombres.Count; j++)
+                {
+                    Console.WriteLine($"Nombre completo: {nombres[j]}");
+                    Console.WriteLine($"Salario bruto: {brutos[j]}");
+                    Console.WriteLine($"Aporte de jubilaciones: {jubilaciones[j]}");
+                    Console.WriteLine($"Aporte de obra social: {obrasociales[j]}");
+                    Console.WriteLine($"Salario neto: {brutos[j] * (1 - jubilaciones[j]) * (1 - obrasociales[j])}");
+                    Console.WriteLine("");
+                }
+            }
+
+
+            
+
         }
     }
 }
---
+++ oop/ejercicios de repaso/Ejerc003-Variedades-01/Ejercicio 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salary = 1200;
+            double hours = 60;
+            double salariobruto = salary * hours;
+            Console.WriteLine($"Su salario bruto es de {salariobruto}");
+            //suponiendo que se aplica uno sobre lo que queda del otro
+            double salarioneto = salariobruto * (1 - 0.11) * (1 - 0.03);
+            Console.WriteLine($"Su salario bruto es de {salarioneto}");
+            //suponiendo que e aplican por separado sobre el salario original
+            //double salarioneto = salariobruto - salariobruto*0.11 - salariobruto*0.03;
+            //
+            //
+        }
+    }
+}
