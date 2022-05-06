c5981e548ff35fc2c9272005fc4e30172884ed78
0horane <leandro.hoarne.siracusa@gmail.com>
2022-05-06 12:03:01 -0300

    tedi start

--- oop/TextEditor/TextEditor/Program.cs
+++ oop/TextEditor/TextEditor/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Ej3
+{
+    class Program
+    {
+
+    
+
+        static void Main(string[] args)
+        {
+
+
+
+            ConsoleKeyInfo pkey;
+            do
+            {
+                //pkey = Console.ReadKey().Key;
+                pkey = Console.ReadKey();
+                switch (pkey.Key)
+                {
+                    case ConsoleKey.Enter:
+                        currenttab.newline();
+                        break
+                    case ConsoleKey.Escape:
+                        currenttab.esc()
+                    case ConsoleKey.Tab:
+                        currenttab.add("    ");
+                    case ConsoleKey.Backspace:
+                        currenttab.rm(-1);
+                    case ConsoleKey.Delete:
+                        currenttab.rm(-1);
+                    case ConsoleKey.RightArrow:
+                        currenttab.movecur(1, 0);
+                    case ConsoleKey.LeftArrow:
+                        currenttab.movecur(-1, 0);
+                    case ConsoleKey.UpArrow:
+                        currenttab.movecur(0, -1);
+                    case ConsoleKey.DownArrow:
+                        currenttab.movecur(0, 1);
+                    default:
+                        currenttab.add(pkey.KeyChar);
+                }
+                currenttab.render();
+
+            } while (true);
+        }
+    }
+}
