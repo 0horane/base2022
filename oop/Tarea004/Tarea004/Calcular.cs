b91762c9f6baa6cd3bf135d6e64b790c6c3851f7
0horane <leandro.horane.siracusa@gmail.com>
2022-06-09 12:49:19 -0300

    fasjil;is

--- oop/Tarea004/Tarea004/Calcular.cs
+++ oop/Tarea004/Tarea004/Calcular.cs
@@ -8,5 +8,30 @@ namespace Tarea004
 {
     static class Calcular
     {
+
+        public static double XTotal(Sucursal[] sucursales, Func<Sucursal, double> sumaque)
+        {
+            double suma = 0;
+            foreach (Sucursal sucursal in sucursales)
+            {
+                suma += sumaque(sucursal);
+            }
+            return suma;
+        }
+
+        public static decimal ventaXima(Sucursal[] sucursales, Func<decimal, decimal, bool> condicionaceptar)
+        {
+            decimal current = sucursales[0].MontoVenta;
+            for (int i = 1; i < sucursales.Length; i++)
+            {
+                if (condicionaceptar(sucursales[i].MontoVenta, current))
+                {
+                    current = i;
+                }
+            }
+            return current;
+        }
+
+
     }
 }
--- oop/Tarea004/Tarea004/Program.cs
+++ oop/Tarea004/Tarea004/Program.cs
@@ -8,11 +8,21 @@ namespace Tarea004 // Note: actual namespace depends on the project name.
 
         static void Main(string[] args)
         {
+            string[] ubicaciones = {
+                "Once",
+                "San Nicolás",
+                "Almagro",
+                "Microcentro",
+                "Palermo",
+                "San Cristóbal",
+                "Parque Patricios"
+            };
+
             Sucursal[] sucursales = new Sucursal[7];
             while (sucursales.Contains(null))
             {
                 int numsucursal=Input.IntC("Que numero de sucursal queres ingresar?", "Eso no es un numero", x=>x>=1 && x<8 && sucursales[x-1] == null , "Sucursal Fuera de rango o Ya ingresada");
-                sucursales[numsucursal-1] = new Sucursal(Input.Decimal("Ingrese el monto total"), Input.Int("Ingrese la cantidad de empleados"), Input.Int("Ingrese la cantidad de "));
+                sucursales[numsucursal-1] = new Sucursal(Input.Decimal("Ingrese el monto total"), Input.Int("Ingrese la cantidad de empleados"), Input.Int("Ingrese la cantidad de Clientes que compraron este mes"));
             }
 
         }
