430c8f3c3b427bfcef22e1afcaecc722308b5c43
0horane <leandro.horane.siracusa@gmail.com>
2022-06-09 12:47:37 -0300

    fasjil;is

---
+++ oop/Tarea004/Tarea004/Input.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea004
+{
+    internal static class Input
+    {
+        private static bool writetrue(string conditionfail)
+        {
+            Console.WriteLine(conditionfail); 
+            return true;
+        }
+        public static int IntC(
+            string prompt = "Ingrese un numero: ",
+            string failprompt = "Eso no es un Numero. Ingrese un numero: ",
+            Func<int, bool>? condition = null,
+            string conditionfail = "Ese número no es valido. Ingrese un numero: ") 
+        {
+            condition = condition ?? (x => true);
+            int ipi;
+            do
+            {
+                ipi = Int(prompt, failprompt);
+            } while (!condition(ipi) && writetrue(conditionfail));
+            return ipi;
+        }
+        public static int Int(
+            string prompt = "Ingrese un numero: ", 
+            string failprompt = "Eso no es un Numero. Ingrese un numero: ")
+        {
+            
+            Console.WriteLine(prompt);
+            int inputnumber;
+            while (!int.TryParse(Console.ReadLine(), out inputnumber))
+                Console.WriteLine(failprompt);
+            return inputnumber;
+        }
+        public static decimal Decimal(
+            string prompt = "Ingrese un numero: ",
+            string failprompt = "Eso no es un Numero. Ingrese un numero: ")
+        {
+
+            Console.WriteLine(prompt);
+            decimal inputnumber;
+            while (!decimal.TryParse(Console.ReadLine(), out inputnumber))
+                Console.WriteLine(failprompt);
+            return inputnumber;
+        }
+    }
+}
---
+++ oop/Tarea004/Tarea004/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tarea004 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Sucursal[] sucursales = new Sucursal[7];
+            while (sucursales.Contains(null))
+            {
+                int numsucursal=Input.IntC("Que numero de sucursal queres ingresar?", "Eso no es un numero", x=>x>=1 && x<8 && sucursales[x-1] == null , "Sucursal Fuera de rango o Ya ingresada");
+                sucursales[numsucursal-1] = new Sucursal(Input.Decimal("Ingrese el monto total"), Input.Int("Ingrese la cantidad de empleados"), Input.Int("Ingrese la cantidad de "));
+            }
+
+        }
+}
+}
