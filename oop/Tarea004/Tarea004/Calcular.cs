3e3ae0ae75e1069237ccc27aba52cdd1a2b5893b
0horane <leandro.horane.siracusa@gmail.com>
2022-06-10 12:01:05 -0300

    tarea004 completadp

--- oop/Tarea004/Tarea004/Calcular.cs
+++ oop/Tarea004/Tarea004/Calcular.cs
@@ -8,30 +8,60 @@ namespace Tarea004
 {
     static class Calcular
     {
+        //#################### metodos internos ###################
 
-        public static double XTotal(Sucursal[] sucursales, Func<Sucursal, double> sumaque)
+        /// <summary>
+        /// Calcula el total de una propiedad de las sucursales
+        /// </summary>
+        /// <param name="sucursales">array de la sucursales</param>
+        /// <param name="sumaque">función que devuelve el parametro deseado de una sucursal</param>
+        /// <returns>suma de todas las de esas propiedades de el array</returns>
+        private static decimal XTotal(Sucursal[] sucursales, Func<Sucursal, decimal> sumaque)
         {
-            double suma = 0;
+            decimal suma = 0;
             foreach (Sucursal sucursal in sucursales)
-            {
                 suma += sumaque(sucursal);
-            }
             return suma;
         }
 
-        public static decimal ventaXima(Sucursal[] sucursales, Func<decimal, decimal, bool> condicionaceptar)
+        /// <summary>
+        /// Compara todos los montos de venta de un array de seucursales
+        /// </summary>
+        /// <param name="sucursales">array de sucursales</param>
+        /// <param name="condicionaceptar">función que compara dos montos (ints) y devuelve true si el mas reciente (primero) es el Ximo (mayor, menor, etc)</param>
+        /// <returns>tuple de el numero de la sucursal correcta y el valor.</returns>
+        private static (int, decimal) ventaXima(Sucursal[] sucursales, Func<decimal, decimal, bool> condicionaceptar, Func<Sucursal, decimal>? Xxima=null)
         {
-            decimal current = sucursales[0].MontoVenta;
+            if (Xxima == null)
+                Xxima = (x => x.MontoVenta);
+            decimal current = Xxima(sucursales[0]);
+            int ri = 0;
             for (int i = 1; i < sucursales.Length; i++)
             {
-                if (condicionaceptar(sucursales[i].MontoVenta, current))
+                if (condicionaceptar(Xxima(sucursales[i]), current))
                 {
-                    current = i;
+                    current = Xxima(sucursales[i]);
+                    ri = i;
                 }
+
             }
-            return current;
+            return (ri, current);
         }
+        //#################### metodos que pide la consigna ###################
+        // sus nombres explican lo que hacen
+        public static decimal ventaTotal(Sucursal[] sucursales) => (XTotal(sucursales, x => x.MontoVenta));
+        public static double promedioVentas(Sucursal[] sucursales) => ((double) ventaTotal(sucursales))/sucursales.Length;
+        public static double promedioEmpleados(Sucursal[] sucursales) => ((double) XTotal(sucursales, x => x.CantidadEmpleados))/sucursales.Length;
+        public static (int, decimal) ventaMaxima(Sucursal[] sucursales) =>ventaXima(sucursales, (x,y)=>x > y);
+        public static (int, decimal) ventaMinima(Sucursal[] sucursales) =>ventaXima(sucursales, (x,y)=>x < y);
+
+
+        //##### metodos para que queden mas lindos los datos en el print ######
+        public static (int, decimal) maxempleados(Sucursal[] sucursales) => ventaXima(sucursales, (x, y) => x > y, x=>x.CantidadEmpleados);
+        public static (int, decimal) maxclienes(Sucursal[] sucursales) => ventaXima(sucursales, (x, y) => x > y, x => x.CantidadEmpleados);
+
 
 
     }
 }
+
--- oop/Tarea004/Tarea004/Input.cs
+++ oop/Tarea004/Tarea004/Input.cs
@@ -8,25 +8,42 @@ namespace Tarea004
 {
     internal static class Input
     {
+        //workaround for something else that wasnt working
         private static bool writetrue(string conditionfail)
         {
             Console.WriteLine(conditionfail); 
             return true;
         }
+
+        /// <summary>
+        /// Ingresa lineas de texto hasta recibir una que es un numero int y que cumple con cierta condidion
+        /// </summary>
+        /// <param name="prompt">texto impreso inicialmente</param>
+        /// <param name="failprompt">texto impreso si se ingresa algo que no es un int</param>
+        /// <param name="condition">condicion</param>
+        /// <param name="conditionfail">texto impreso si el texto ingresado no cumple con la condicion</param>
+        /// <returns>Int que cumple con la condicion</returns>
         public static int IntC(
             string prompt = "Ingrese un numero: ",
             string failprompt = "Eso no es un Numero. Ingrese un numero: ",
             Func<int, bool>? condition = null,
             string conditionfail = "Ese número no es valido. Ingrese un numero: ") 
         {
-            condition = condition ?? (x => true);
+            condition = condition ?? (x => true); //no se permite poner una funcion anonima como parametro por defecto.
             int ipi;
             do
             {
                 ipi = Int(prompt, failprompt);
+                // esta linea no me gusta nada. solo imprime el caso de fallo de condicion luegod el primer bucle aprovechando que solo se evalua la segunda parte de un and si la primera es falsa.
             } while (!condition(ipi) && writetrue(conditionfail));
             return ipi;
         }
+        /// <summary>
+        /// Ingresa lineas de texto hasta recibir una que es un numero int 
+        /// </summary>
+        /// <param name="prompt">texto impreso inicialmente</param>
+        /// <param name="failprompt">texto impreso si se ingresa algo que no es un int</param>
+        /// <returns>numero ingresado</returns>
         public static int Int(
             string prompt = "Ingrese un numero: ", 
             string failprompt = "Eso no es un Numero. Ingrese un numero: ")
@@ -38,6 +55,13 @@ namespace Tarea004
                 Console.WriteLine(failprompt);
             return inputnumber;
         }
+
+        /// <summary>
+        /// Ingresa lineas de texto hasta recibir una que es un numero decimal
+        /// </summary>
+        /// <param name="prompt">texto impreso inicialmente</param>
+        /// <param name="failprompt">texto impreso si se ingresa algo que no es un decimal</param>
+        /// <returns>numero ingresado</returns>
         public static decimal Decimal(
             string prompt = "Ingrese un numero: ",
             string failprompt = "Eso no es un Numero. Ingrese un numero: ")
